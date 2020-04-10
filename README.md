c9f0783b4115248c97bd776516fbef0c6eb99e41
Derek Williams <derek61@gmail.com>
2020-04-10 11:22:27 -0600

    added gis filter plugins, updated README

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # covid-19-graphql
 Simple postgraphile based graphql API based on the data from Johns Hopkins, see https://github.com/CSSEGISandData/COVID-19.git
+
+## Note 
+
+The database is stored in the postgres Docker container, 
+so to start fresh delete that container first, otherwise the init scripts will not run
+
+## Postgraphile Plugins Used (installed globally for now)
+
+Connection Filter
+
+https://github.com/graphile-contrib/postgraphile-plugin-connection-filter
+
+https://www.npmjs.com/package/postgraphile-plugin-connection-filter
+
+Postgis
+
+https://github.com/graphile/postgis
+
+https://www.npmjs.com/package/@graphile/postgis
+
+Postgis Filter
+
+https://github.com/graphile-contrib/postgraphile-plugin-connection-filter-postgis
+
+https://www.npmjs.com/package/postgraphile-plugin-connection-filter-postgis
+
+## Example Queries
+
+Getting case counts in the US
+
+```{
+  allLocations (filter: {iso3: {equalTo: "USA"}}) {
+    nodes {
+      centroid {
+        geojson
+        srid
+        x
+        y
+      }
+      fips
+      admin2
+      iso3
+      code3
+      combinedKey
+      caseCountsByLocationId {
+        nodes {
+          count
+        }
+      }
+    }
+  }
+}
+```
+
--- db/init/00-database.sql
+++ db/init/00-database.sql
@@ -3,13 +3,56 @@
 
 CREATE SCHEMA johns_hopkins;
 
-    
+CREATE EXTENSION IF NOT EXISTS postgis schema public;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" schema public;
 
 CREATE TABLE johns_hopkins.location (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    uid int8,
+    iso2 varchar(2),
+    iso3 varchar(3),
+    code3 int4,
+    fips varchar(8),
+    admin2 varchar(128),
+    province_state varchar(128),
+    country_region varchar(128),
+    combined_key varchar(256),
+    centroid geometry(POINT, 4326),
     name text
 );
+
+
+
+COMMENT ON TABLE johns_hopkins.location IS
+'A bit denormalized, matches the columns in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.uid IS
+'This is a nique ID in Johns Hopkins time series data, can be used to match the rows from the Johns Hopkins data sets, Same column as uid in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.iso2 IS
+'Same column as iso2 in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.iso3 IS
+'Same column as iso3 in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.code3 IS
+'Same column as code33 in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.fips IS
+'Same column as fips in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.admin2 IS
+'Same column as admin2 in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.province_state IS
+'Same column as province_state in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.country_region IS
+'Same column as country_region in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
+COMMENT ON COLUMN johns_hopkins.location.combined_key IS
+'Same column as combined_key in https://github.com/derekjwilliams/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
+
 ALTER TABLE johns_hopkins.location ADD CONSTRAINT location_pkey PRIMARY KEY (id);
 
 CREATE TABLE johns_hopkins.case_count (
@@ -21,7 +64,6 @@ CREATE TABLE johns_hopkins.case_count (
 ALTER TABLE johns_hopkins.case_count ADD CONSTRAINT case_count_pkey PRIMARY KEY (id);
 
 ALTER TABLE johns_hopkins.case_count ADD CONSTRAINT case_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
-
 
 COMMENT ON TABLE johns_hopkins.case_count IS
 'Confirmed Counts';
