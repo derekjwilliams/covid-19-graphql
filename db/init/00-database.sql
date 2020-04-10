9b0c6e61f9bf5b8716d21375bcd457fc2844e8a5
Derek Williams <derek61@gmail.com>
2020-04-10 10:22:12 -0600

    fix db init sql

--- db/init/00-database.sql
+++ db/init/00-database.sql
@@ -1,35 +1,26 @@
+/*Connecting to the database automatically creates it*/
 \connect covid
 
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-
 CREATE SCHEMA johns_hopkins;
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
-CREATE SCHEMA johns_hopkins;
+    
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" schema public;
 
 CREATE TABLE johns_hopkins.location (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     name text
 );
+ALTER TABLE johns_hopkins.location ADD CONSTRAINT location_pkey PRIMARY KEY (id);
 
 CREATE TABLE johns_hopkins.case_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     location_id uuid,
     "time" TIMESTAMPTZ not NULL,
     "count" INT8
  );
-ALTER TABLE johns_hopkins.case_count ADD CONSTRAINT casecount_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
+ALTER TABLE johns_hopkins.case_count ADD CONSTRAINT case_count_pkey PRIMARY KEY (id);
+
+ALTER TABLE johns_hopkins.case_count ADD CONSTRAINT case_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
 
 
 COMMENT ON TABLE johns_hopkins.case_count IS
@@ -40,11 +31,15 @@ COMMENT ON COLUMN johns_hopkins.case_count.count IS
 
 
 CREATE TABLE johns_hopkins.missing_case_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
-    location_id INT4,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    location_id uuid,
     "missing" Bool default false,
     "missing_reason" varchar (32)
 );
+
+ALTER TABLE johns_hopkins.missing_case_count ADD CONSTRAINT missing_case_count_pkey PRIMARY KEY (id);
+
+ALTER TABLE johns_hopkins.missing_case_count ADD CONSTRAINT missing_case_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
 
 COMMENT ON COLUMN johns_hopkins.missing_case_count.missing IS
 'Indicates if if data is missing, false if valid data is present';
@@ -54,12 +49,14 @@ COMMENT ON COLUMN johns_hopkins.missing_case_count.missing_reason IS
 
 
 CREATE TABLE johns_hopkins.death_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     location_id uuid,
     "time" TIMESTAMPTZ not NULL,
     "count" INT8
  );
-ALTER TABLE johns_hopkins.death_count ADD CONSTRAINT deathcount_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
+
+ALTER TABLE johns_hopkins.death_count ADD CONSTRAINT death_count_pkey PRIMARY KEY (id);
+ALTER TABLE johns_hopkins.death_count ADD CONSTRAINT death_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
 
 
 COMMENT ON TABLE johns_hopkins.death_count IS
@@ -68,13 +65,16 @@ COMMENT ON TABLE johns_hopkins.death_count IS
 COMMENT ON COLUMN johns_hopkins.death_count.count IS
 'Number of deaths, if missing also check missing and missing_reason';
 
-
 CREATE TABLE johns_hopkins.missing_death_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
-    location_id INT4,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    location_id uuid,
     "missing" Bool default false,
     "missing_reason" varchar (32)
 );
+
+ALTER TABLE johns_hopkins.missing_death_count ADD CONSTRAINT missing_death_count_pkey PRIMARY KEY (id);
+ALTER TABLE johns_hopkins.missing_death_count ADD CONSTRAINT missing_death_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
+
 
 COMMENT ON COLUMN johns_hopkins.missing_death_count.missing IS
 'Indicates if if data is missing, false if valid data is present';
@@ -83,12 +83,13 @@ COMMENT ON COLUMN johns_hopkins.missing_death_count.missing_reason IS
 'Reason for missing data';
 
 CREATE TABLE johns_hopkins.recovered_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     location_id uuid,
     "time" TIMESTAMPTZ not NULL,
     "count" INT8
  );
-ALTER TABLE johns_hopkins.recovered_count ADD CONSTRAINT recoveredcount_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
+ALTER TABLE johns_hopkins.recovered_count ADD CONSTRAINT recovered_count_pkey PRIMARY KEY (id);
+ALTER TABLE johns_hopkins.recovered_count ADD CONSTRAINT recovered_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
 
 
 COMMENT ON TABLE johns_hopkins.recovered_count IS
@@ -99,15 +100,17 @@ COMMENT ON COLUMN johns_hopkins.recovered_count.count IS
 
 
 CREATE TABLE johns_hopkins.missing_recovered_count (
-    id uuid DEFAULT mimi.uuid_generate_v4() NOT NULL,
-    location_id INT4,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    location_id uuid,
     "missing" Bool default false,
     "missing_reason" varchar (32)
 );
+
+ALTER TABLE johns_hopkins.missing_recovered_count ADD CONSTRAINT missing_recovered_count_pkey PRIMARY KEY (id);
+ALTER TABLE johns_hopkins.missing_recovered_count ADD CONSTRAINT missing_recovered_count_location_id_fkey FOREIGN KEY (location_id) REFERENCES johns_hopkins.location(id) ON UPDATE CASCADE;
 
 COMMENT ON COLUMN johns_hopkins.missing_recovered_count.missing IS
 'Indicates if if data is missing, false if valid data is present';
 
 COMMENT ON COLUMN johns_hopkins.missing_recovered_count.missing_reason IS
 'Reason for missing data';
-
