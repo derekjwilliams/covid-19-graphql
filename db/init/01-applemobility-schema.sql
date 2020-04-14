e645b728d4d7251632f6a81f0b42e28ddf83cc58
Derek Williams <derek61@gmail.com>
2020-04-14 12:51:52 -0600

    rename init files, add apple mobility init schema sql

---
+++ db/init/01-applemobility-schema.sql
@@ -0,0 +1,16 @@
+/*Connecting to the database automatically creates it*/
+\connect covid
+
+CREATE SCHEMA apple_mobility;
+
+CREATE EXTENSION IF NOT EXISTS postgis schema public;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" schema public;
+
+CREATE TABLE apple_mobility.mobility_location_type (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    geo_type varchar(64),
+    region varchar(64),
+    transportation_type varchar(64),
+);
+
+-- See https://www.apple.com/covid19/mobility for more information
