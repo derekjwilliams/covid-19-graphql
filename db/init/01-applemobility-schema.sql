22e5d119b88faf058cca8363ccf83ddec0eaf006
Derek Williams <derek61@gmail.com>
2020-04-15 14:58:05 -0600

    fix single quotes

--- db/init/01-applemobility-schema.sql
+++ db/init/01-applemobility-schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE apple_mobility.mobility_location_type (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     geo_type varchar(64),
     region varchar(64),
-    transportation_type varchar(64),
+    transportation_type varchar(64)
 );
 
 -- See https://www.apple.com/covid19/mobility for more information
