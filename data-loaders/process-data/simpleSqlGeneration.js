1ccc85e4cb0a3dbc48d0caefc8c45b0e0eeeaf7f
Jason Redcedar <jasonredcedar@gmail.com>
2020-04-15 18:38:29 -0700

    Adding deaths data

--- data-loaders/process-data/simpleSqlGeneration.js
+++ data-loaders/process-data/simpleSqlGeneration.js
@@ -9,11 +9,16 @@ dotenv.config()
 // const GlobalNonDateHeaderString ='UID,iso2,iso3,code3,FIPS,Admin2,Province_State,Country_Region,Lat,Long_,Combined_Key,Population'
 
 const defaultPopulationsOrigin = '../../../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv' // just to get locations with populations
+const defaultDeathsOrigin = '../../../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv' // just to get locations with populations
+
 const defaultLocationsInsertDestination = '../../db/init/50-johnshopkins-location-data.sql'
+const defaultDeathsInsertDestination = '../../db/init/51-johnshopkins-death-data.sql'
 
 const populationsOrigin = process.env.POPULATIONS_FILENAME || defaultPopulationsOrigin
-const locationsInsertDestination = process.env.LOCATIONS_DESTINATION || defaultLocationsInsertDestination
+const deathsOrigin = process.env.DEATHS_FILENAME || defaultDeathsOrigin
 
+const locationsInsertDestination = process.env.LOCATIONS_DESTINATION || defaultLocationsInsertDestination
+const deathsInsertDestination = process.env.DEATHS_DESTINATION || defaultDeathsInsertDestination
 
 const locations = new Map()
 const lines = fs.readFileSync(populationsOrigin, {encoding: 'utf8'}).split('\n')
@@ -74,11 +79,6 @@ const validHeader = lines[0].startsWith(usNonDateHeaderString)
 const validDataLength = verifyDataLength(lines);
 
 if (validHeader && validDataLength) {
-  //ISOString for the stated of day for the date in the csse date, e.g. 2/23/20 is 2020-02-23T00:00:00.000Z
-  const dates = lines[0]
-                  .split(',')
-                  .filter((header, index) => index > usNonDateHeaderString.split(',').length)
-                  .map(rawDate => moment.utc(rawDate, "MM/DD/YY").toISOString())
   const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/
 
   // const filename = '../../db/init/50-johnshopkins-location-data.sql'
@@ -87,41 +87,84 @@ if (validHeader && validDataLength) {
       console.log('error' + err)
       throw err;
     }
-    console.log(`created ${locationsInsertDestination}`);
-  });
-  fs.appendFile(locationsInsertDestination, '\\connect covid;\n\n', function (err) {
-    if (err) {
-      throw err;
-    }
-  });
-
-  lines.forEach((line, index) => {
-    if (index) {
-      const locationData = line.split(regex).filter((value,index) => index < usNonDateHeaderString.split(',').length)
-      const countData = line.split(regex).filter((value,index) => index >= usNonDateHeaderString.split(',').length)
-      if (locationData.length > 1) {
-        const locationDataWrapped = wrapLocationDataStringParts(locationData)
-        const id = uuid.v4()
-        const lat = locationDataWrapped[indexOfLat]
-        const lon = locationDataWrapped[indexOfLon]
-        locationDataWrapped.splice(indexOfLat, 2, "ST_GeomFromText('POINT(" + lon + " " + lat + ")', 4326)")
-        locations.set(locationDataWrapped[0], locationDataWrapped)
-        
-        // write  location insert sql, the locations Map above is used in the next step (after lines.forEach) to write to deaths and confirmed insert file
-        const locationInsert = `INSERT INTO johns_hopkins.location(${databaseColumns}) VALUES ('${id}',${locationDataWrapped.join(",")});\n`
-
-        fs.appendFile(locationsInsertDestination, locationInsert, function (err) {
-          if (err) {
-            throw err;
-          }
-        });
+    fs.appendFile(locationsInsertDestination, '\\connect covid;\n\n', function (err) {
+      if (err) {
+        throw err;
       }
-    }
+    });
+    lines.forEach((line, index) => {
+      if (index) {
+        const locationData = line.split(regex).filter((value,index) => index < usNonDateHeaderString.split(',').length)
+        
+        if (locationData.length > 1) {
+          const locationDataWrapped = wrapLocationDataStringParts(locationData)
+          const id = uuid.v4()
+          const lat = locationDataWrapped[indexOfLat]
+          const lon = locationDataWrapped[indexOfLon]
+  
+          locationDataWrapped.splice(indexOfLat, 2, "ST_GeomFromText('POINT(" + lon + " " + lat + ")', 4326)")
+         
+          // add our generated UUID to the location data too
+          locations.set(locationDataWrapped[0], locationDataWrapped.unshift(`${id}`))
+          
+          // write  location insert sql, the locations Map above is used in the next step (after lines.forEach) to write to deaths and confirmed insert file
+          const locationInsert = `INSERT INTO johns_hopkins.location(${databaseColumns}) VALUES (${locationDataWrapped.join(",")});\n`
+  
+          fs.appendFile(locationsInsertDestination, locationInsert, function (err) {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+  
+      }
+    })
+    console.log(`created ${locationsInsertDestination}`);
+    fs.close(file, (err) => {
+      if (err) throw err;
+    })
   })
 
-  // at this point locations Map contains all of the locations, lines contains all of the death data.
-  // TODO Open and process time_series_covid_confirmed_US, at this point in time (April 18) there is no US recovered file 
-  // using the UID from the locations Map
-  // TODO read .env file to get list of files to process
 
+
+  // TODO break this into its own function
+  const deathLines = fs.readFileSync(deathsOrigin, {encoding: 'utf8'}).split('\n')
+  
+  //ISOString for the stated of day for the date in the csse date, e.g. 2/23/20 is 2020-02-23T00:00:00.000Z
+                  
+  const dates = deathLines[0]
+                  .split(',')
+                  .filter((header, index) => index >= usNonDateHeaderString.split(',').length)
+                  .map(rawDate => moment.utc(rawDate, "MM/DD/YY").toISOString())
+
+  fs.open(deathsInsertDestination, 'w', function (err, file) {
+
+    deathLines.forEach((line,index) => {
+      
+      const UID = line.split(',')[0]
+      let counts = line.split(',').slice()    
+      counts.splice(0, keys.length + 2)
+      
+      if (counts.length === dates.length) {
+        counts.forEach((count, ci) => {
+          const countId = uuid.v4()
+          const deathInsert = `INSERT INTO johns_hopkins.death_count(id,location_id,time,count) VALUES ('${countId}',${UID},'${dates[ci]}',${count});\n`
+
+          fs.appendFile(deathsInsertDestination, deathInsert, function (err) {
+            if (err) {
+              throw err;
+            }
+          })
+
+        })
+      }
+    })
+
+    fs.close(file, (err) => {
+      if (err) throw err;
+    })
+
+  })
+
+  
 }
