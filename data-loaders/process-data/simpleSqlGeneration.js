3227af9f7f6307c2815c83b905742b854d6ab02a
Derek Williams <derek61@gmail.com>
2020-04-17 09:34:26 -0600

    simplify a little, comments

--- data-loaders/process-data/simpleSqlGeneration.js
+++ data-loaders/process-data/simpleSqlGeneration.js
@@ -52,7 +52,7 @@ const keys = Array.from(locationHeaderToSqlColumns.keys())
 const indexOfLat = keys.indexOf('Lat')
 const indexOfLon = keys.indexOf('Long_')
 
-const usNonDateHeaderString = keys.join(',');
+const usNonDateHeaderString = keys.join(',')
 const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/
 
 const wrapLocationDataStringParts = (locationData) => {
@@ -90,8 +90,8 @@ const wrapLocationDataStringParts = (locationData) => {
   }
 */
 const createLocationsMap = (data) => {
-  const lines = data.split('\n')
   const result = new Map()
+  const lines = data.split('\n')
   lines.forEach((line, index) => {
     if (index) {
       const locationData = line.split(regex).filter((_,index) => index < usNonDateHeaderString.split(',').length)
@@ -100,15 +100,14 @@ const createLocationsMap = (data) => {
         const lat = locationDataWrapped[indexOfLat]
         const lon = locationDataWrapped[indexOfLon]
 
-        // add centroid, replacing LAT and LONG_
+        // add centroid point value, replacing LAT and LONG_
         locationDataWrapped.splice(indexOfLat, 2, "ST_GeomFromText('POINT(" + lon + " " + lat + ")', 4326)")
         
         // add a UUID V4, which is used for the location id in all of the data inserts
         locationDataWrapped.unshift(`'${uuid.v4()}'`)
         const values = {}
         locationSqlColumns.forEach((key,index) => values[key] = locationDataWrapped[index])
-        const uid = locationDataWrapped[1]
-        result.set(uid, values)
+        result.set(locationDataWrapped[1], values)
       }
     }
   })
@@ -125,26 +124,25 @@ const createLocationInserts = (locationsMap = {}) => {
   return result
 }
 
-const createCountInserts = (locationsMap = {}, data = {}, tableName = 'none') => {
+const createCountInserts = (locationsMap = {}, data = '', tableName = 'none') => {
   const result = []
   const lines = data.split('\n')
   const header = lines[0]
   const headers = header.split(regex)
   const last = headers.indexOf('Population') > 0 ? headers.indexOf('Population') : headers.indexOf('Combined_Key')
   
-  const dates = header.split(regex)
-                      .filter((_,index) => index > last)
-                      .map(rawDate => moment.utc(rawDate, 'MM/DD/YY').toISOString())
+  // create iso dates from input date columns
+  const dates = headers.filter((_,index) => index > last)
+                       .map(d => moment.utc(d, 'MM/DD/YY').toISOString())
 
   lines.forEach((line, index) => {
     if (index) {
-      const location = locationsMap.get(line.split(',')[0])
+      const location = locationsMap.get(line.split(',')[0]) // uid (johns hopkins unique id) is in the 0th column of the counts csv
       if (location !== undefined) {
         const counts = line.split(regex).filter((_,index) => index > last)
         if (counts.length === dates.length) {
-          const id = location['id']
           dates.forEach((date, index) => 
-            result.push( `INSERT INTO johns_hopkins.${tableName}(${countSqlColumns}) VALUES ('${uuid.v4()}',${id},'${date}', ${counts[index]});`)            
+            result.push( `INSERT INTO johns_hopkins.${tableName}(${countSqlColumns}) VALUES ('${uuid.v4()}',${location['id']},'${date}', ${counts[index]});`)            
           )
         }
       }
