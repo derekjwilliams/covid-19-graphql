23929bb8e69265458de1d9705c691b7985212e7b
Derek Williams <derek61@gmail.com>
2020-04-22 17:52:48 -0600

    simple fetch client

---
+++ graphql/simpleClient/simpleFetchClient.js
@@ -0,0 +1,3 @@
+const fetch = require('node-fetch');
+
+fetch("http://localhost:5000/graphql", {"credentials":"include","headers":{"accept":"application/json","accept-language":"en-US,en;q=0.9,fr;q=0.8","content-type":"application/json","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrer":"http://localhost:5000/graphiql","referrerPolicy":"no-referrer-when-downgrade","body":"{\"query\":\"{\\n  allLocations(first: 2) {\\n    nodes {\\n      centroid {\\n        geojson\\n        srid\\n        x\\n        y\\n      }\\n\\t\\t\\tdeathCountsByLocationId {\\n        totalCount\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"projectReference\":{\"projectReference\":{\"projectId\":\"3\"}}}}","method":"POST","mode":"cors"});
