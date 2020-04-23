628e6825fc0e98226d89a56e26ab6362215d9b11
Derek Williams <derek61@gmail.com>
2020-04-22 20:03:23 -0600

    example of using fetch RequestInit in d3 json request

--- graphql/simpleClient/simpleFetchClient.js
+++ graphql/simpleClient/simpleFetchClient.js
@@ -1,9 +1,37 @@
-const fetch = require('node-fetch');
+const d3 = require("d3-fetch")
+const gql = require("graphql-tag")
 
-fetch("http://localhost:5000/graphql", {"credentials":"include","headers":{"accept":"application/json","accept-language":"en-US,en;q=0.9,fr;q=0.8","content-type":"application/json","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrer":"http://localhost:5000/graphiql","referrerPolicy":"no-referrer-when-downgrade","body":"{\"query\":\"{\\n  allLocations(first: 2) {\\n    nodes {\\n      centroid {\\n        geojson\\n        srid\\n        x\\n        y\\n      }\\n\\t\\t\\tdeathCountsByLocationId {\\n        totalCount\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"projectReference\":{\"projectReference\":{\"projectId\":\"3\"}}}}","method":"POST","mode":"cors"})
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log(JSON.stringify(data, null, 2))
-  })
+if (typeof fetch !== 'function') {
+  global.fetch = require('node-fetch-polyfill');
+}
+const g = gql`{
+  allLocations(first: 1) {
+    nodes {
+      deathCountsByLocationId {
+        totalCount
+      }
+    }
+  }
+}
+`
+
+const query = `{"query":${JSON.stringify(g.loc.source.body)}}`
+d3.json("http://localhost:5000/graphql", {
+  credentials: "include",
+  headers: {
+    accept: "application/json",
+    "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+    "content-type": "application/json",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+  },
+  referrer: "http://localhost:5000/graphiql",
+  referrerPolicy: "no-referrer-when-downgrade",
+  body:
+  query,
+  method: "POST",
+  mode: "cors",
+}).then((data) => {
+  console.log(JSON.stringify(data, null, 2));
+});
