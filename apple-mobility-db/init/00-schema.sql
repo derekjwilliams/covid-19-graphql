a1794edad5eb68613eb7059257cc0760e0ec25a6
Derek Williams <derek61@gmail.com>
2020-04-30 14:40:33 -0600

    update for mobility

--- apple-mobility-db/init/00-schema.sql
+++ apple-mobility-db/init/00-schema.sql
@@ -1,6 +1,3 @@
-/*Connecting to the database automatically creates it*/
-\connect covid
-
 CREATE SCHEMA apple_mobility;
 
 CREATE EXTENSION IF NOT EXISTS postgis schema public;
@@ -10,7 +7,8 @@ CREATE TABLE apple_mobility.location (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     geo_type varchar(64),
     region varchar(64),
-    transportation_type varchar(64)
+    transportation_type varchar(64),
+    alternative_name varchar(64)
 );
 
 ALTER TABLE apple_mobility.location ADD CONSTRAINT location_pkey PRIMARY KEY (id);
@@ -25,5 +23,3 @@ CREATE TABLE apple_mobility.value (
 ALTER TABLE apple_mobility.value ADD CONSTRAINT value_pkey PRIMARY KEY (id);
 
 ALTER TABLE apple_mobility.value ADD CONSTRAINT value_location_id_fkey FOREIGN KEY (location_id) REFERENCES apple_mobility.location(id) ON UPDATE CASCADE;
-
--- See https://www.apple.com/covid19/mobility for more information
--- apple-mobility-db/process-data/simpleSqlCreate.js
+++ apple-mobility-db/process-data/simpleSqlCreate.js
@@ -4,9 +4,47 @@ import uuid from "uuid"
 import dotenv from "dotenv"
 dotenv.config()
 
-const locationsInsertDestination = "./01-locations.sql";
-const valuesInsertDestination = "./02-values.sql";
+const origin = '../applemobilitytrends.csv'
+const locationsInsertDestination = "../init/01-locations.sql";
+const valuesInsertDestination = "../init/02-values.sql";
+const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+const valueInserts = []
 
-const processUSData = async () => {
-  const rawPopulationData = await fsPromises.readFile(usPopulationsOrigin,"utf8")
+const addValueInserts = (locationId, data, dates) => {
+  const values = data.slice()
+  values.splice(0,4)
+  const reals = values.map(v => +v)
+  if (reals.length > 0 && !isNaN(reals[0])) {
+    const inserts = reals.map((r,i) => 
+    `INSERT INTO apple_mobility.value(id,location_id,time,value) VALUES ('${uuid.v4()}','${locationId}','${dates[i]}',${r});`)
+    inserts.forEach(insert => {
+      valueInserts.push(insert)
+    })
+  }
 }
+
+const processData = async () => {
+  const locationData = await fsPromises.readFile(origin,"utf8")
+  const lines = locationData.split('\n')
+  const dates = lines[0].split(regex)
+  dates.splice(0,4)
+  const locationInserts = lines.map((line, index) => {
+    const data = line.split(regex)
+    if (data.length > 3) {
+      let location = data[1] !== undefined ? data[1].replace(/^"|"$/g, '') : ''
+      location = location.replace("'", "''");
+      let alternativeName = data[3] !== undefined ? data[3].replace(/^"|"$/g, '') : ''
+      alternativeName = alternativeName.replace("'", "''");
+      const geoType = data[0]
+      const transportationType = data[2]
+      const id = uuid.v4()
+      addValueInserts(id, data, dates)
+      return `INSERT INTO apple_mobility.location(id,geo_type,region,transportation_type,alternative_name) VALUES ('${id}','${geoType}','${location}','${transportationType}','${alternativeName}');`
+    }
+    return ''
+  }).filter(insertStatement => insertStatement !== '')
+  await fsPromises.writeFile(locationsInsertDestination,locationInserts.join('\n'))
+  await fsPromises.writeFile(valuesInsertDestination, valueInserts.join('\n'))
+}
+
+processData()
